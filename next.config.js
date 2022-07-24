a794d09966e4ab0a27fb4471ee258d89d3d6a903
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2022-07-24 21:00:08 +0100

    +experiment.layers: true

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: function (config, options) {
-    config.experiments = { asyncWebAssembly: true };
+    config.experiments = { asyncWebAssembly: true, layers: true };
     return config;
-}
+  }
 }
 
 module.exports = nextConfig
