6415adbf0d95f9df333e3a218e8cec0e870e287f
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2022-08-01 23:52:25 +0100

    +status in my raffles

--- components/MyRaffles.js
+++ components/MyRaffles.js
@@ -9,13 +9,13 @@ function MyRaffles({ baseAddress }) {
         fetch(`https://lottery.easystaking.online/raffles/${baseAddress}?status=joined`)
             .then((res) => res.json())
             .then((data) => data.filter(raffle => !raffle.is_closed))
-            .then((openRaffles) => openRaffles.map(raffle => [raffle.epoch, `${raffle.prize} ₳`, `${raffle.min_stake} ₳`, raffle.num_participants]))
+            .then((openRaffles) => openRaffles.map(raffle => [raffle.epoch, `${raffle.prize} ₳`, `${raffle.min_stake} ₳`, raffle.num_participants, "Open"]))
             .then((data) => setJoinedRaffles(data))
 
     }, [])
 
     return (
-        <Table title="My Raffles" columnNames={["Epoch", "Prize", "Stake Req.", "Participants"]} rows={joinedRaffles} />
+        <Table title="My Raffles" columnNames={["Epoch", "Prize", "Stake Req.", "Participants", "Status"]} rows={joinedRaffles} />
     )
 }
 
