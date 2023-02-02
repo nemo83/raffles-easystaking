f9a3c9cc61fb594b2a88e0c6848152ffc1193ea4
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-02-02 21:53:50 +0000

    staking address

--- components/Perks.js
+++ components/Perks.js
@@ -7,6 +7,12 @@ export default function Perks() {
             <TextTile title="Title">
                 <p> Hello worldasd</p>
             </TextTile>
+            <TextTile title="Title">
+                <p> Hello worldasd</p>
+            </TextTile>
+            <TextTile title="Title">
+                <p> Hello worldasd</p>
+            </TextTile>
         </div>
     )
 
--- components/TextTile.js
+++ components/TextTile.js
@@ -1,13 +1,13 @@
-export default function TextTile({ children, title}) {
+export default function TextTile({ children, title }) {
 
     return (
         <div className="w-full p-3 md:w-1/2 xl:w-1/3">
 
-            <div className="bg-gray-900 border border-gray-800 rounded shadow">
-                <div className="p-3 border-b border-gray-800">
-                    <h5 className="font-bold text-gray-600 uppercase">{title}</h5>
+            <div className="border border-black rounded shadow bg-sky-600">
+                <div className="p-3 border-b border-black">
+                    <h5 className="font-bold uppercase text-grey-300">{title}</h5>
                 </div>
-                <div className="p-5 text-gray-400">
+                <div className="p-5 text-slate-50">
                     {children}
                 </div>
             </div>
