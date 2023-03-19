3f719a4e64136846bcbf83cdef5968253892fc8b
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-03-19 11:26:34 +0000

    env vars

--- constants/lottery.ts
+++ constants/lottery.ts
@@ -1,2 +1,2 @@
-export const optimizeSmartContracts = true
+export const optimizeSmartContracts = process.env.NEXT_PUBLIC_OPTIMIZE_SCRIPT == 'true'
 export const lotteryApi = "https://lottery.easystaking.online"
