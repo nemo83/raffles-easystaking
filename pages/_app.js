fb94d1c58f6539a2a812109bec6719cb24bdb122
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-03-20 18:57:43 +0000

    testnet

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
+import { network } from "../constants/blockfrost"
+import { config as HeliosConfig } from "@hyperionbt/helios"
+HeliosConfig.IS_TESTNET = network.toString() != 'mainnet'
+
+
 export default function MyApp({ Component, pageProps }) {
 
   return (
