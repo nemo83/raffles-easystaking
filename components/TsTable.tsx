679bfabfd8a614ee16d187fecf9e69109f8da0aa
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-11-15 11:06:06 +0000

    chore: update name properties

--- components/TsTable.tsx
+++ components/TsTable.tsx
@@ -141,8 +141,8 @@ Find out more at easy1staking.com
                                 stroke="currentColor"
                                 className="w-6 h-6">
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
